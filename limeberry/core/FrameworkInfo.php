b3eaa3cecbfc7306d4c092c538abfc5798b3f154
ezoheux <46251064+ezoheux@users.noreply.github.com>
2018-12-30 21:45:46 -0500

    Update FrameworkInfo.php

--- limeberry/core/FrameworkInfo.php
+++ limeberry/core/FrameworkInfo.php
@@ -1,35 +1,39 @@
 <?php
+/**
+ * Limeberry Framework
+ *   
+ * A php framework for fast web development.
+ *   
+ * @package Limeberry Framework
+ * @author Sinan SALIH
+ * @copyright Copyright (C) 2018-2019 Sinan SALIH
+ */
+
+namespace limeberry\core
 
 /**
-*	Limeberry Framework
-*	
-*	a php framework for fast web development.
-*	
-*	@package Limeberry Framework
-*	@author Sinan SALIH
-*	@copyright Copyright (C) 2018-2019 Sinan SALIH
-*	
-**/
-namespace limeberry\core
+ * FrameworkInfo class contains information and version data of the framework. 
+ */
+class FrameworkInfo
 {
+        
     /**
-     * FrameworkInfo class contains information and version data of the framework. 
+     * Return the version of the framework.
+     *
+     * @return string Returns version number of Limeberry PHP Framework.
      */
-    class FrameworkInfo{
-        
-        /**
-         * Returns version number of Limeberry PHP
-         */
-        public static function VersionNumber(){
-            return "2.0.0@stable";
-        }
-        
-        /**
-         * Last configuration on core framework files of the Limeberry
-         */
-        public static function LastUpdate(){
-            return "2.0@1.1 Update -  January First Update ";
-        }
+    public static function VersionNumber()
+    {
+        return "2.0.0@stable";
     }
     
+    /**
+     * Last configuration on core framework files of the Limeberry PHP Framework.
+     *
+     * @return string Returns the last configuration on core framework files of the Limeberry PHP Framework.
+     */
+    public static function LastUpdate()
+    {
+        return "2.0@1.1 Update -  January First Update ";
+    }
 }
