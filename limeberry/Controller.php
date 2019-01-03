44f0b779cf5e6ca889934869137bc3e46bea1b74
ezoheux <46251064+ezoheux@users.noreply.github.com>
2019-01-03 07:52:30 -0500

    Update Controller.php

--- limeberry/Controller.php
+++ limeberry/Controller.php
@@ -1,99 +1,98 @@
 <?php
+/**
+ * Limeberry Framework.
+ *
+ * A php framework for fast web development.
+ *
+ * @author Sinan SALIH
+ * @copyright Copyright (C) 2018-2019 Sinan SALIH
+ */
+
+namespace limeberry;
+
+use limeberry\core\Content;
 
 /**
- *	Limeberry Framework.
- *
- *	a php framework for fast web development.
- *
- *	@author Sinan SALIH
- *	@copyright Copyright (C) 2018-2019 Sinan SALIH
- *
- **/
-
-namespace limeberry
+ * Controller class of Limeberry framework.
+ */
+class Controller
 {
-
-    use limeberry\core\Content;
-
+    
+    private $current_version = '';
+    private $controller_author = '';
+    
     /**
-     * Controller class of Limeberry framework.
+     * Initialize.
+     *
+     * @return void Returns nothing.
      */
-    class Controller
+    public function __construct()
     {
-        private $current_version = '';
-        private $controller_author = '';
-
-        /**
-         * Initialize.
-         */
-        public function __construct()
-        {
-            $this->View = new View();
-            $this->Content = new Content();
-        }
-
-        /**
-         * Set version for your controller.
-         *
-         * @param mixed $version
-         *
-         * @return $this
-         */
-        public function setVersion($version)
-        {
-            $this->current_version = $version;
-
-            return $this;
-        }
-
-        /**
-         * Set author to the controller.
-         *
-         * @param string $fullname
-         *
-         * @return $this
-         */
-        public function setAuthor($fullname)
-        {
-            $this->controller_author = $fullname;
-
-            return $this;
-        }
-
-        /**
-         * Get controller's version number.
-         *
-         * @return string
-         */
-        public function getVersion()
-        {
-            return $this->current_version;
-        }
-
-        /**
-         * Get controller's author.
-         *
-         * @return string
-         **/
-        public function getAuthor()
-        {
-            return $this->controller_author;
-        }
-
-        /**
-         * This function is used to return an array of defined functions of your controller.
-         */
-        protected function getFunctions()
-        {
-            return get_class_methods($this);
-        }
-
-        /**
-         * This function is used to return an array of defined variables of your controller.
-         */
-        protected function getVariables()
-        {
-            return get_defined_vars();
-        }
+        $this->View    = new View();
+        $this->Content = new Content();
+    }
+    
+    /**
+     * Set version for your controller.
+     *
+     * @param mixed $version
+     *
+     * @return $this
+     */
+    public function setVersion($version)
+    {
+        $this->current_version = $version;
+        
+        return $this;
+    }
+    
+    /**
+     * Set author to the controller.
+     *
+     * @param string $fullname
+     *
+     * @return $this
+     */
+    public function setAuthor($fullname)
+    {
+        $this->controller_author = $fullname;
+        
+        return $this;
+    }
+    
+    /**
+     * Get controller's version number.
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        return $this->current_version;
+    }
+    
+    /**
+     * Get controller's author.
+     *
+     * @return string
+     **/
+    public function getAuthor()
+    {
+        return $this->controller_author;
+    }
+    
+    /**
+     * This function is used to return an array of defined functions of your controller.
+     */
+    protected function getFunctions()
+    {
+        return get_class_methods($this);
+    }
+    
+    /**
+     * This function is used to return an array of defined variables of your controller.
+     */
+    protected function getVariables()
+    {
+        return get_defined_vars();
     }
 }
