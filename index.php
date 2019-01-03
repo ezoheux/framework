abfcc98c8924f6791f6943c1118d29f56b10882b
ezoheux <46251064+ezoheux@users.noreply.github.com>
2019-01-03 08:23:16 -0500

    Update and rename public/index.php to index.php

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
  * @author Sinan SALIH
  * @copyright Copyright (C) 2018-2019 Sinan SALIH
  */
-$path = __DIR__.'/../vendor/autoload.php';
+$path = __DIR__.'/vendor/autoload.php';
 if (file_exists($path)) {
     require $path;
 } else {
-    require __DIR__.'/../autoload.php';
+    require __DIR__.'/autoload.php';
 }
-require __DIR__.'/../index.php';
+require __DIR__.'/run.php';
