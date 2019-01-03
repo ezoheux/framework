3758fe6cf13c6f00bbbfa2e66d64d339019f1ecb
ezoheux <46251064+ezoheux@users.noreply.github.com>
2019-01-03 05:26:11 -0500

    Update app_config.php

--- app_config.php
+++ app_config.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Limeberry Framework
+ *   
+ * A php framework for fast web development.
+ *   
+ * @package Limeberry Framework
+ * @author Sinan SALIH
+ * @copyright Copyright (C) 2018-2019 Sinan SALIH
+ */
+
 use limeberry\Configuration;
 
 /**
