42e1d0506ed90c01d72e51354a558e07149ad25b
ezoheux <46251064+ezoheux@users.noreply.github.com>
2019-01-03 05:33:38 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,12 @@
+<?php
+/**
+ * Limeberry Framework
+ *   
+ * A php framework for fast web development.
+ *   
+ * @package Limeberry Framework
+ * @author Sinan SALIH
+ * @copyright Copyright (C) 2018-2019 Sinan SALIH
+ */
 
+require __DIR__ . '/../vendor/autoload.php';
