41bbb7c5b2e509138e4500fc1df766e004b202b4
ezoheux <46251064+ezoheux@users.noreply.github.com>
2019-01-03 18:07:37 -0500

    Update View.php

--- limeberry/View.php
+++ limeberry/View.php
@@ -10,6 +10,8 @@
 
 namespace limeberry;
 
+defined("START") || (header("HTTP/1.1 403 Forbidden") & die("403.14 - Directory listing denied."));
+
 use limeberry\Configuration as conf;
 use const true;
 use function file_exists;
