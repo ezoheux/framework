13995d7345d593f5f598d4ce6af4e254397d3eda
ezoheux <46251064+ezoheux@users.noreply.github.com>
2019-01-03 05:14:32 -0500

    Create base.php

---
+++ base.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Limeberry Framework
+ *   
+ * A php framework for fast web development.
+ *   
+ * @package Limeberry Framework
+ * @author Sinan SALIH
+ * @copyright Copyright (C) 2018-2019 Sinan SALIH
+ */
+
+/** 
+ * Directory Separator Constant
+ */
+define('DS', DIRECTORY_SEPARATOR);
+
+/** 
+ * Root Directory of current running php file.
+ */
+define('ROOT', dirname(dirname(__FILE__)));
