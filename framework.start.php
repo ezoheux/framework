be73084fe9281bfb36531a337fdc335dc6eaf3cb
ezoheux <46251064+ezoheux@users.noreply.github.com>
2019-01-03 18:05:47 -0500

    Update framework.start.php

--- framework.start.php
+++ framework.start.php
@@ -7,6 +7,8 @@
  * @author Sinan SALIH
  * @copyright Copyright (C) 2018-2019 Sinan SALIH
  */
+defined("START") || (header("HTTP/1.1 403 Forbidden") & die("403.14 - Directory listing denied."));
+
 use limeberry\Framework;
 
 // app_config.php is the configuration file for your project.
