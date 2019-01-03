d6bfaafe6117b4b533aef0533071e3b10e32bec4
ezoheux <46251064+ezoheux@users.noreply.github.com>
2019-01-03 07:47:54 -0500

    Update ComposerTest.php

--- tests/ComposerTest.php
+++ tests/ComposerTest.php
@@ -1,1 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
 
+class ComposerTest extends TestCase
+{
+    public function testFrameworkLoadedThroughComposer()
+    {
+        $this->assertTrue(defined('FRAMEWORK_LOADED'));
+    }
+}
